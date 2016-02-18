30c38db71dd04089a9d9a2a3313bceecce53f033
Tanner Andersen <tanner.andersen14@gmail.com>
2016-02-18 11:55:02 -0800

    ok

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ myApp.controller('MainController', function($scope, mySvc) {
 
   $scope.getUsers = function() {
      mySvc.getUsers().then(function(response) {
-       $scope.users = response.data.data;
+       $scope.users = response;
      });
    }
   $scope.getUsers();
